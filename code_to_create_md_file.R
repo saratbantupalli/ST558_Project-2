5b69fc4cfb9ee1fbb0026e75b8075c80c81d5bad
saratbantupalli <sarat212@gmail.com>
2023-10-11 20:49:04 -0400

    vignette commit

---
+++ code_to_create_md_file.R
@@ -0,0 +1,5 @@
+# Use this code to create the README.md file
+
+rmarkdown::render(input = "Project2.Rmd", output_format = "github_document", output_file = "README.md")
+
+# Output for github pages can be found here: https://saratbantupalli.github.io/ST558_Project-2/
